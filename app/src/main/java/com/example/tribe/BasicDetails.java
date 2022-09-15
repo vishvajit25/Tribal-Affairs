8b7b9871af8b29a61e1df8624dc0af362a93f3fa
vishvajit25 <vishvajit25@gmail.com>
2022-09-15 11:58:18 +0530

    Tribal_affairs App

--- app/src/main/java/com/example/tribe/BasicDetails.java
+++ app/src/main/java/com/example/tribe/BasicDetails.java
@@ -13,13 +13,18 @@ import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.text.TextUtils;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -42,8 +47,10 @@ import java.util.ArrayList;
 
 public class BasicDetails extends AppCompatActivity {
     Button next, pic;
+    String[] genderarr = {"MALE", "FEMALE","PREFER NOT TO SAY"};
     ImageView image;
-    EditText name;
+    EditText etname,fid,aadhar,phoneno,organization;
+    AutoCompleteTextView gender;
     FirebaseStorage storage;
     StorageReference storageReference;
     FusedLocationProviderClient client;
@@ -64,9 +71,26 @@ public class BasicDetails extends AppCompatActivity {
         setContentView(R.layout.activity_basic_details);
         next = findViewById(R.id.next);
         image = findViewById(R.id.imageview);
-        name = findViewById(R.id.name);
+        etname = findViewById(R.id.name1);
+        aadhar = findViewById(R.id.aadhar);
+        phoneno = findViewById(R.id.phoneno);
+        organization = findViewById(R.id.org);
+        gender = findViewById(R.id.gender);
+        fid = findViewById(R.id.familyid1);
         storage = FirebaseStorage.getInstance();
         storageReference = storage.getReference();
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(),
+                android.R.layout.simple_selectable_list_item,genderarr);
+        gender.setAdapter(adapter);
+        gender.setThreshold(1);
+        gender.setTextColor(Color.BLACK);
+        gender.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                gender.showDropDown();
+                return false;
+            }
+        });
         pic = findViewById(R.id.pic);
         mStorageRef = FirebaseStorage.getInstance().getReference();
         pic.setOnClickListener(new View.OnClickListener() {
@@ -85,34 +109,49 @@ public class BasicDetails extends AppCompatActivity {
                             ,Manifest.permission.ACCESS_COARSE_LOCATION},100);                }
             }
         });
+        String name=etname.getText().toString();
+        String aadharno = aadhar.getText().toString();
+        String famid=fid.getText().toString();
+        String phonenumber=phoneno.getText().toString();
+        String org=organization.getText().toString();
+        String genderstr = gender.getText().toString();
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String  str = name.getText().toString();
-                StorageReference imagenameref = mStorageRef.child(str+".jpg");
-                image.setDrawingCacheEnabled(true);
-                image.buildDrawingCache();
-                Bitmap bitmap = ((BitmapDrawable) image.getDrawable()).getBitmap();
-                ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
-                byte[] data = baos.toByteArray();
+                if (name==null) {
+                    etname.setError("Name is required.");
+                }
+                if (famid==null) {
+                    fid.setError("FamilyID cant be empty.");
+                }
+                else {
+                    String str = etname.getText().toString();
+                    String str2 = fid.getText().toString();
+                    StorageReference imagenameref = mStorageRef.child(str + str2 + ".jpg");
+                    image.setDrawingCacheEnabled(true);
+                    image.buildDrawingCache();
+                    Bitmap bitmap = ((BitmapDrawable) image.getDrawable()).getBitmap();
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+                    byte[] data = baos.toByteArray();
 
-                UploadTask uploadTask = imagenameref.putBytes(data);
-                uploadTask.addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception exception) {
-                        // Handle unsuccessful uploads
-                    }
-                }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                    @Override
-                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                        Toast.makeText(BasicDetails.this,"IMAGE UPLOADED SUCCESSFULLY !", Toast.LENGTH_SHORT).show();
-                        // taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
-                        // ...
-                    }
-                });
-                Intent intent = new Intent(BasicDetails.this, PersonalDetails.class);
-                startActivity(intent);
+                    UploadTask uploadTask = imagenameref.putBytes(data);
+                    uploadTask.addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception exception) {
+                            // Handle unsuccessful uploads
+                        }
+                    }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Toast.makeText(BasicDetails.this, "IMAGE UPLOADED SUCCESSFULLY !", Toast.LENGTH_SHORT).show();
+                            // taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
+                            // ...
+                        }
+                    });
+                    Intent intent = new Intent(BasicDetails.this, PersonalDetails.class);
+                    startActivity(intent);
+                }
             }
         });
 
